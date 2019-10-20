188c4ebf40cde4d6344add17cbe6c5b514a2c4d2
Mathieu Pont <matthieu.pont@hotmail.fr>
2019-10-20 16:21:02 +0200

    work on make_[week/day/parking] dataset

--- Algorithms/util.R
+++ Algorithms/util.R
@@ -5,16 +5,29 @@ pause <- function(){
   readline(prompt="Press [enter] to continue") 
 }
 
-make_week_dataset <- function(data){
-  # Add week number and weekday to group data
-  data$LastUpdated_week_hours<-strftime(data$LastUpdated_rounded,format="%w.%H:%M")
-  data$LastUpdated_week<-strftime(as.POSIXlt(data$LastUpdated, tz = "Europe/Amsterdam"),format="%W")
+make_week_dataset <- function(data, num_parking, num_week){
+  # # Add week number and weekday to group data
+  # data$LastUpdated_week_hours<-strftime(data$LastUpdated_rounded,format="%w.%H:%M")
+  # data$LastUpdated_week<-strftime(as.POSIXlt(data$LastUpdated, tz = "Europe/Amsterdam"),format="%W")
+  # 
+  # grouped_data=group_by(data,SystemCodeNumber,LastUpdated_week, LastUpdated_week_hours)
+  # grouped_data <- grouped_data %>% summarise(
+  #   Occupancy = sum(Occupancy)
+  # )
+  # final_matrix <- dcast(grouped_data, SystemCodeNumber + LastUpdated_week ~ LastUpdated_week_hours, value.var = "Occupancy")
+  # #final_matrix[1,]
   
-  grouped_data=group_by(data,SystemCodeNumber,LastUpdated_week, LastUpdated_week_hours)
-  grouped_data <- grouped_data %>% summarise(
-    Occupancy = sum(Occupancy)
-  )
-  final_matrix <- dcast(grouped_data, SystemCodeNumber + LastUpdated_week ~ LastUpdated_week_hours, value.var = "Occupancy")
-  #final_matrix[1,]
+  final_matrix <- matrix(data[,3], nrow = num_parking*num_week, byrow = TRUE)
+  return(final_matrix)
+}
+
+
+make_day_dataset <- function(data, num_parking, num_day){
+  final_matrix <- matrix(data[,3], nrow = num_parking*num_day, byrow = TRUE)
+  return(final_matrix)
+}
+
+make_parking_dataset <- function(data, num_parking){
+  final_matrix <- matrix(data[,3], nrow = num_parking, byrow = TRUE)
   return(final_matrix)
 }
